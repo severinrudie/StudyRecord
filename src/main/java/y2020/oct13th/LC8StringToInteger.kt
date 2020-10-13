718b2677d41041072cb625e355795b5c662646cc
severinrudie <severinrudie@gmail.com>
2020-10-13 15:44:14 -0700

    8-StringToInteger

---
+++ src/main/java/y2020/oct13th/LC8StringToInteger.kt
@@ -0,0 +1,37 @@
+package y2020.oct13th
+
+import java.lang.RuntimeException
+
+object LC8StringToInteger {
+    fun myAtoi(s: String): Int {
+        val regex = """^\s*([+-]?)(\d+).*""".toRegex()
+        val match = regex.matchEntire(s)
+
+        val rawSign = match?.groups?.get(1)?.value
+        val sign = if (rawSign == "" || rawSign == null) "+" else rawSign
+        val value = match?.groups?.get(2)?.value?.trimStart('0') ?: return 0
+
+        if (sign.length != 1) return 0
+
+        if (value.length > 10) {
+            return if (sign == "+") 2147483647
+            else -2147483648
+        }
+        if (value.isEmpty()) return 0
+        if (value.length == 10) {
+            val max = when (sign) {
+                "+" -> "2147483647"
+                "-" -> "2147483648"
+                else -> throw RuntimeException()
+            }
+
+            val paired = value.zip(max)
+            val firstUnequal = paired.indexOfFirst { it.first != it.second }
+            val firstTooBig = paired.indexOfFirst { it.first > it.second }
+
+            if (firstTooBig != -1 && (firstUnequal >= firstTooBig)) return (sign + max).toInt()
+        }
+
+        return (sign + value).toInt()
+    }
+}
---
+++ src/test/java/y2020/oct13th/LC8StringToIntegerTest.kt
@@ -0,0 +1,94 @@
+package y2020.oct13th
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import y2020.oct13th.LC8StringToInteger.myAtoi
+
+internal class LC8StringToIntegerTest {
+    @Test
+    fun tc1() {
+        assertEquals(-42, myAtoi("-42"))
+    }
+
+    @Test
+    fun tc2() {
+        assertEquals(-42, myAtoi("    -42"))
+    }
+
+    @Test
+    fun tc3() {
+        assertEquals(-42, myAtoi("    -42aasdf"))
+    }
+
+    @Test
+    fun tc4() {
+        assertEquals(42, myAtoi("    42"))
+    }
+
+    @Test
+    fun tc5() {
+        assertEquals(0, myAtoi("f    -42aasdf"))
+    }
+
+    @Test
+    fun tc6() {
+        assertEquals(-42, myAtoi("    -42 aasdf   "))
+    }
+
+    @Test
+    fun tc7() {
+        assertEquals(-2147483648, myAtoi("-91283472332"))
+    }
+
+    @Test
+    fun tc8() {
+        assertEquals(-2147483648, myAtoi("-2147483649"))
+        assertEquals(-2147483648, myAtoi("-2147483648"))
+    }
+
+    @Test
+    fun tc9() {
+        assertEquals(2147483647, myAtoi("2147483648"))
+        assertEquals(2147483647, myAtoi("2147483647"))
+    }
+
+    @Test
+    fun tc10() {
+        assertEquals(42, myAtoi("+42"))
+    }
+
+    @Test
+    fun tc11() {
+        assertEquals(4193, myAtoi("4193 with words"))
+    }
+
+    @Test
+    fun tc12() {
+        assertEquals(-2147483648, myAtoi("-91283472332"))
+    }
+
+    @Test
+    fun tc13() {
+        assertEquals(2147483647, myAtoi("91283472332"))
+    }
+
+    @Test
+    fun tc14() {
+        assertEquals(0, myAtoi("+-12"))
+    }
+
+    @Test
+    fun tc15() {
+        assertEquals(12345678, myAtoi("  0000000000012345678"))
+    }
+
+    @Test
+    fun tc16() {
+        assertEquals(0, myAtoi("00000-42a1234"))
+    }
+
+    @Test
+    fun tc17() {
+        assertEquals(1095502006, myAtoi("1095502006p8"))
+    }
+}
