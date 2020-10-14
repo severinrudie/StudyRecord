aed78535012512d6e12a30acedcedaa98b88df3b
severinrudie <severinrudie@gmail.com>
2020-10-14 08:47:32 -0700

    9-PalindromeNumber

---
+++ src/main/java/y2020/oct14th/LC9PalindromeNumber.kt
@@ -0,0 +1,23 @@
+package y2020.oct14th
+
+// Easy, 48% acceptance
+// O(n) time, O(n) space
+object LC9PalindromeNumber {
+    fun isPalindrome(x: Int): Boolean {
+        if (x < 0) return false
+        var mutX = x
+        val digits = mutableListOf<Int>()
+        while (mutX > 0) {
+            val next = mutX % 10
+            mutX = (mutX - next) / 10
+            digits += next
+        }
+
+        val steps = if (x % 2 == 0) digits.size / 2 else (digits.size / 2) + 1
+        for (front in 0 until steps) {
+            val back = digits.lastIndex - front
+            if (digits[front] != digits[back]) return false
+        }
+        return true
+    }
+}
