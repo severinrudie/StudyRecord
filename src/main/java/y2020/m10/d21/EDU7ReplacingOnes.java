f8c0c36f48d45aa85ff6df907306d042f2b8aeed
severinrudie <severinrudie@gmail.com>
2020-10-21 13:58:00 -0700

    EDU7-ReplacingOnes

---
+++ src/main/java/y2020/m10/d21/EDU7ReplacingOnes.java
@@ -0,0 +1,22 @@
+package y2020.m10.d21;
+
+public class EDU7ReplacingOnes {
+    public static int findLength(int[] arr, int k) {
+        int longest = 0;
+        int currZeros = 0;
+        int left = 0;
+
+        for (int right = 0; right < arr.length; right++) {
+            if (arr[right] == 0) currZeros++;
+
+            while (currZeros > k && left < right) {
+                if (arr[left] == 0) currZeros--;
+                left++;
+            }
+
+            longest = Math.max(longest, right - left + 1);
+        }
+
+        return longest;
+    }
+}
