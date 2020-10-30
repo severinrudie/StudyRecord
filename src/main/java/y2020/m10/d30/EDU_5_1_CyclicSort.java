525d113f6e43124326c208284caba61f4981fb32
severinrudie <severinrudie@gmail.com>
2020-10-30 13:27:54 -0700

    EDU_5_1_CyclicSort

---
+++ src/main/java/y2020/m10/d30/EDU_5_1_CyclicSort.java
@@ -0,0 +1,18 @@
+package y2020.m10.d30;
+
+public class EDU_5_1_CyclicSort {
+    public static void sort(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int temp = nums[i];
+            if (temp == i + 1) {
+                // no swap needed
+                i++;
+            } else {
+                // swap, and keep i where it is to follow up
+                nums[i] = nums[temp - 1];
+                nums[temp - 1] = temp;
+            }
+        }
+    }
+}
