e4e5237c7ab4f01f0851e5ebc0756b32694af5c6
severinrudie <severinrudie@gmail.com>
2020-11-04 12:28:46 -0800

    Added basic TreeNode data structure

---
+++ src/main/java/shared/TreeNode.kt
@@ -0,0 +1,77 @@
+package shared
+
+import kotlin.math.max
+
+data class TreeNode(val `val`: Int, val left: TreeNode?, val right: TreeNode?) {
+    companion object {
+        fun from(vararg list: List<Int>): TreeNode {
+            val last = mutableListOf<TreeNode>()
+            val next = mutableListOf<TreeNode>()
+
+            for (row in list.asList().asReversed()) {
+                for (num in row) {
+                    val left = last.removeFirstOrNull()
+                    val right = last.removeFirstOrNull()
+
+                    next += TreeNode(num, left, right)
+                }
+                last.clear()
+                last.addAll(next)
+                next.clear()
+            }
+            return last.first()
+        }
+    }
+
+    fun maxWidth() = toList().maxOf { it.size }
+
+    fun toList(): List<List<Int>> = toList(mutableMapOf(), 1)
+
+    private fun toList(
+            lists: MutableMap<Int, List<Int>>,
+            currDepth: Int
+    ): List<List<Int>> {
+        lists[currDepth] = lists.getOrDefault(currDepth, emptyList()) + `val`
+
+        left?.toList(lists, currDepth + 1)
+        right?.toList(lists, currDepth + 1)
+
+        return lists.values.toList()
+    }
+
+    fun maxDepth() = maxDepth(1)
+
+    private fun maxDepth(currDepth: Int): Int = max(
+        left?.maxDepth(currDepth + 1) ?: currDepth,
+        right?.maxDepth(currDepth + 1) ?: currDepth
+        )
+
+    // TODO does not handle numbers with multiple digits well
+    override fun toString(): String {
+        val list = toList()
+        val maxDepth = maxDepth()
+
+        var result = ""
+
+        for (i in maxDepth - 1 downTo 0) {
+            val lead = 2.pow(i)
+            val buffer = " ".repeat(lead - 1)
+            result += list[list.lastIndex - i].map { "$buffer$it$buffer" }
+                    .joinToString(separator = " ", prefix = " ", postfix = " ") +
+                    "\n"
+        }
+
+        return result.replaceAfterLast('\n', "")
+    }
+}
+
+/**
+ * Can easily overflow
+ */
+private fun Int.pow(exp: Int): Int {
+    var result = 1
+    for (i in 1..exp) {
+        result *= this
+    }
+    return result
+}
---
+++ src/test/java/shared/TreeNodeTest.kt
@@ -0,0 +1,79 @@
+package shared
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import kotlin.math.pow
+
+internal class TreeNodeTest {
+    @Test
+    fun tc1() {
+        val tn = TreeNode.from(
+                listOf(1),
+                listOf(2,3),
+                listOf(4,5,6,7),
+                listOf(8,9,10),
+                listOf(11)
+        )
+
+        assertEquals(4, tn.maxWidth())
+    }
+
+    @Test
+    fun tc2() {
+        val tn = TreeNode.from(
+                listOf(1),
+                listOf(2,3),
+                listOf(4,5,6,7),
+                listOf(8,9,10),
+                listOf(11)
+        )
+
+        assertEquals(5, tn.maxDepth())
+    }
+
+    @Test
+    fun tc3() {
+
+        val tn = TreeNode.from(
+                listOf(1),
+                listOf(2,3),
+                listOf(4,5,6,7),
+                listOf(8,9,10),
+                listOf(11)
+        )
+
+        assertEquals(
+                """
+                |                1                
+                |        2               3        
+                |    4       5       6       7    
+                |  8   9   10  
+                | 11 
+                |
+                """.trimMargin()
+                ,
+                tn.toString()
+        )
+    }
+
+    @Test
+    fun tc4() {
+        val tn = TreeNode.from(
+                listOf(1),
+                listOf(2,3),
+                listOf(4,5,6,7),
+                listOf(8,9,10),
+                listOf(11)
+        ).toList()
+
+        val lists = listOf(
+                listOf(1),
+                listOf(2,3),
+                listOf(4,5,6,7),
+                listOf(8,9,10),
+                listOf(11)
+        )
+
+        assertEquals(lists, tn)
+    }
+}
