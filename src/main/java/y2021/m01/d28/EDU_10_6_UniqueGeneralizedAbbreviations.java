df86cc6f76115e4c7a78990db284328133005a5a
severinrudie <severinrudie@gmail.com>
2021-01-29 10:24:18 -0800

    Cleaned up EDU_10_6_UniqueGeneralizedAbbreviations

--- src/main/java/y2021/m01/d28/EDU_10_6_UniqueGeneralizedAbbreviations.java
+++ src/main/java/y2021/m01/d28/EDU_10_6_UniqueGeneralizedAbbreviations.java
@@ -1,46 +1,45 @@
 package y2021.m01.d28;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class EDU_10_6_UniqueGeneralizedAbbreviations {
     // O(n 2^n) time, O(n 2^n) space
-    public static List<String> generateGeneralizedAbbreviation(String word) {
-        Set<String> result = new HashSet<String>(); // TODO how can I do this without cheating by using a set?
-        result.add(word);
+    public static List<String> generateGeneralizedAbbreviation(String target) {
+        return generateGeneralizedAbbreviation(target, "");
+    }
 
-        List<String> lastRun = new ArrayList<>(result);
-        while (!lastRun.isEmpty()) {
-            String next = lastRun.remove(0);
-
-            for (int i = 0; i < next.length(); i++) {
-                if (Character.isDigit(next.charAt(i))) continue;
-                boolean preNum = i > 0 && Character.isDigit(next.charAt(i - 1));
-                boolean postNum = i < next.length() - 1 && Character.isDigit(next.charAt(i + 1));
-                int preI = i;
-                if (preNum) preI--;
-                if (preI < 0) preI = 0;
-                int postI = i + 1;
-                if (postNum) postI++;
-
-                String pre = next.substring(0, preI);
-                int code = 1;
-                if (preNum) code += Character.getNumericValue(next.charAt(i - 1));
-                if (postNum) code += Character.getNumericValue(next.charAt(i + 1));
-                String post = "";
-                if (postI < next.length()) post = next.substring(postI);
-
-                String newStr = pre +
-                        code +
-                        post;
-
-                result.add(newStr);
-                if (newStr.length() > 1) lastRun.add(newStr);
-            }
+    public static List<String> generateGeneralizedAbbreviation(String target, String curr) {
+        if (target.length() == curr.length()) {
+            List<String> result = new LinkedList<>();
+            result.add(withUnderscoresCounted(curr));
+            return result;
         }
 
-        return new ArrayList<>(result);
+        String withNum = curr + '_';
+        curr += target.charAt(curr.length());
+
+        List<String> result = generateGeneralizedAbbreviation(target, curr);
+        result.addAll(generateGeneralizedAbbreviation(target, withNum));
+
+        return result;
+    }
+
+    static String withUnderscoresCounted(String input) {
+        StringBuilder result = new StringBuilder();
+        int num = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == '_') {
+                num += 1;
+            } else {
+                if (num > 0) {
+                    result.append(num);
+                    num = 0;
+                }
+                result.append(input.charAt(i));
+            }
+        }
+        if (num > 0) result.append(num);
+
+        return result.toString();
     }
 }
--- src/test/java/y2021/m01/d28/EDU_10_6_UniqueGeneralizedAbbreviations_Test.kt
+++ src/test/java/y2021/m01/d28/EDU_10_6_UniqueGeneralizedAbbreviations_Test.kt
@@ -8,8 +8,16 @@ internal class EDU_10_6_UniqueGeneralizedAbbreviations_Test {
 
     @Test
     fun tc1() {
-        val expect = listOf("BAT", "BA1", "B1T", "B2", "1AT", "1A1", "2T", "3", "hi").sorted()
+        val expect = listOf("BAT", "BA1", "B1T", "B2", "1AT", "1A1", "2T", "3").sorted()
         val actual = generateGeneralizedAbbreviation("BAT").sorted()
+        assertEquals(expect, actual)
+    }
+
+    @Test
+    fun tc2() {
+        val expect = listOf("code", "cod1", "co1e", "co2", "c1de", "c1d1", "c2e", "c3", "1ode", "1od1", "1o1e", "1o2",
+            "2de", "2d1", "3e", "4").sorted()
+        val actual = generateGeneralizedAbbreviation("code").sorted()
         assertEquals(expect, actual)
     }
 }
