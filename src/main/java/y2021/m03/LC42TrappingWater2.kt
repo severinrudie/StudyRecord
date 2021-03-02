48e8f4a9cf75b13a2607de76887d0bdf19defd30
severinrudie <severinrudie@gmail.com>
2021-03-02 15:27:34 -0800

    LC42TrappingWater (two pointer version)

---
+++ src/main/java/y2021/m03/LC42TrappingWater2.kt
@@ -0,0 +1,29 @@
+package y2021.m03
+
+import kotlin.math.max
+
+class LC42TrappingWater2 {
+    // Two pointer solution
+    // O(n) time, O(1) space
+    fun trap(height: IntArray): Int {
+        if (height.isEmpty()) return 0
+        var r = height.lastIndex
+        var l = 0
+        var rMax = height[r]
+        var lMax = height[l]
+        var total = 0
+
+        while (l < r) {
+            if (height[l] < height[r]) {
+                lMax = max(lMax, height[l])
+                total += lMax - height[l]
+                l++
+            } else {
+                rMax = max(rMax, height[r])
+                total += rMax - height[r]
+                r--
+            }
+        }
+        return total
+    }
+}
