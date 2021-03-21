c53babc5451ee09900c86f1eb4538f9cb550ce93
severinrudie <severinrudie@gmail.com>
2021-03-21 09:22:16 -0700

    LC729MyCalendar1

---
+++ src/main/java/y2021/m03/d21/LC729MyCalendar1.kt
@@ -0,0 +1,28 @@
+package y2021.m03.d21
+
+import java.util.*
+
+class LC729MyCalendar1 {
+    private val cal = TreeSet<IntRange> { a, b -> a.start - b.start }
+
+    // O(log n) time, O(n) space
+    fun book(start: Int, end: Int): Boolean {
+        val floor = cal.floor(start..end)
+        val ceil = cal.ceiling(start..end)
+
+        val exists = floor == start..end || ceil == start..end
+        val conflictDown = floor != null && floor.endInclusive > start
+        val conflictUp = ceil != null && ceil.start < end
+
+        return if (
+            exists ||
+            conflictDown ||
+            conflictUp
+        ) {
+            false
+        } else {
+            cal.add(start..end)
+            true
+        }
+    }
+}
