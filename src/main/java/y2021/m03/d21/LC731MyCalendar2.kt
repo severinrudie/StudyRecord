250f2b38c2079774cd7dcc5291c98797a0ecc3b7
severinrudie <severinrudie@gmail.com>
2021-03-21 11:20:47 -0700

    LC731MyCalendar2

---
+++ src/main/java/y2021/m03/d21/LC731MyCalendar2.kt
@@ -0,0 +1,27 @@
+package y2021.m03.d21
+
+import java.util.*
+
+class LC731MyCalendar2 {
+    class MyCalendarTwo() {
+
+        val tm = TreeMap<Int, Int>()
+
+        // O(n) time, O(1) space
+        fun book(start: Int, end: Int): Boolean {
+            tm[start] = (tm[start] ?: 0) + 1
+            tm[end] = (tm[end] ?: 0) - 1
+
+            var total = 0
+            for ((k,v) in tm.subMap(0, end)) {
+                total += v
+                if (total > 2) {
+                    tm[start] = tm[start]!! - 1
+                    tm[end] = tm[end]!! + 1
+                    return false
+                }
+            }
+            return true
+        }
+    }
+}
